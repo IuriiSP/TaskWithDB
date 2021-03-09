15dc21189abaef8d8de3e5e0c0c36f6fca67e799
IuriiSP <42968697+IuriiSP@users.noreply.github.com>
2021-03-09 16:13:44 +0300

    test

--- src/main/java/com/iurii/mysql/Application.java
+++ src/main/java/com/iurii/mysql/Application.java
@@ -2,7 +2,7 @@ package com.iurii.mysql;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+// without hibernate
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
